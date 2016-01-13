f46740ce0ed77db782aca5d914cad88822523701
Raymond <raymond_squared@live.com>
2016-01-13 20:05:36 +1100

    JSX and ES6

--- Crash Courses Express and ES6/webpack.config.js
+++ Crash Courses Express and ES6/webpack.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-	entry: "./public/js/app.js",
-	output: {
+module.exports = 
+{
+	entry: "./js/app.js",
+	output: 
+	{
 		path: __dirname + "/public",
 		filename: "bundle.js"
+	},
+	module:
+	{
+		loaders:
+		[
+			{
+				test: /\js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: 
+				{
+			    	presets: ['react', 'es2015']
+			    }
+			}
+		]
 	}
 }
